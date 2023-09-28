d782050bca0e88ec2ff9277588c7fa818e2c8e10
fabiogarbato <82621006+fabiogarbato@users.noreply.github.com>
2023-09-28 11:48:48 -0300

    Criação de uma tabela para os tipo de Cursos, e aplicação de clean code

--- src/pages/Sobre/sobre.js
+++ src/pages/Sobre/sobre.js
@@ -1,18 +1,23 @@
 import './sobre.css'
+import TabelaCursos from './tabelaCursos.js'
 import { Link, useLocation } from 'react-router-dom';
 
 function Sobre(){
+
     const location = useLocation();
+
     const titulos = [
         'UMA BREVE APRESENTAÇÃO',
         'MUITA PESQUISA E PROFISSIONALISMO',
-        'ONDE ATUAMOS E NOSSAS PARCERIAS'
+        'ONDE ATUAMOS E NOSSAS PARCERIAS',
+        'CONFIRA ABAIXO NOSSAS OPÇÕES DE CURSOS E CONSULTORIA'
     ]
     const textos = [
         'Somos uma empresa de consultoria e treinamento. Nosso principal foco é capacitar clientes com a nossa expertise. Compartilhar experiências e principalmente ensinar nossas técnicas do nível básico ao avançado.',
         'O cidadão brasileiro é prejudicado reiteradamente com informações desencontradas no quesito arma de fogo. Por esta razão, criamos um projeto inovador ao realizar um levantamento mais assertivo. Com uma base sólida de conhecimento, a Armory Consulting foi criada para promover consultoria, cursos e treinamento no crescente mercado nacional de armas.',
         'Atualmente exercemos nossos cursos exclusivamente em clubes de tiro. Curitiba e região metropolitana. Firmamos parcerias com despachantes, instrutores, estandes e armeiros credenciados. Isso garante que o profissional contratado esteja em conformidade com as normas e exigências da Polícia Federal e do Exército. A parceria com as lojas de armas e materiais táticos, fornece um vasto acervo para apresentarmos aos nossos clientes, diversos tipos de armas nacionais e importadas.'
       ];
+
     return(      
         <div className='container'>
             <div className='header_site'>  
@@ -31,13 +36,16 @@ function Sobre(){
                     <div className='title-text-column-1'> {titulos[0]}
                         <div className='text-column-1'>{textos[0]}</div>
                     </div>
+                    <div className='title-text2-column-1'> {titulos[3]}
+                        <TabelaCursos/>
+                    </div>
                 </div>
                 <div className='column-2'> 
                     <div className='title-text-column-2'> {titulos[1]}
                         <div className='text-column-2'>{textos[1]}</div>
                     </div>
-                    <div className='title-text-column-2'> {titulos[2]}
-                        <div className='text-column-2'>{textos[2]}</div>
+                    <div className='title-text2-column-2'> {titulos[2]}
+                        <div className='text2-column-2'>{textos[2]}</div>
                     </div>
                 </div>
             </div>
---
+++ src/pages/Sobre/tabelaCursos.js
@@ -0,0 +1,71 @@
+import React from 'react';
+import TableContainer from '@mui/material/TableContainer';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableRow from '@mui/material/TableRow';
+import TableCell from '@mui/material/TableCell'; 
+import { styled } from '@mui/material/styles'
+
+const CustomTableTitleCell = styled(TableCell)`
+  font-size: 18px; 
+  color: rgb(255, 115, 0); 
+  font-family: Arial, sans-serif; 
+`;
+
+const CustomTableTextCell = styled(TableCell)`
+  font-size: 18px; 
+  color: rgb(255, 255, 255); 
+  font-family: Arial, sans-serif; 
+`;
+
+function TabelaCursos() {
+
+  const op_nivel = [
+    'BASICO',
+    'INTERMEDIARIO',
+    'AVANÇADO',
+    'CONSULTORIA'
+  ] 
+  
+  const op_consultoria = [
+    'Ideal para iniciantes acima de 18 anos, com baixa ou nenhuma experiência com armas de fogo. Optando por esporte ou defesa pessoal.',
+    'Para atiradores que já possuem um nível de conhecimento elevado de armas curtas. Querendo aprimorar seu aprendizado no emprego de armas portáteis.',
+    'Treinamento tático para civil ou forças armadas. Abrangendo destreza, disciplina e técnicas avançadas de armas semi-automáticas e de repetição.',
+    'Iniciamos os trâmites no momento do aceite do cliente em adquirir uma arma de fogo, até sua conclusão final. Com seriedade e competência.'
+  ] 
+
+  return (
+    <TableContainer>
+      <Table>
+          <TableBody>
+          <TableRow>
+              <CustomTableTitleCell variant="head">{op_nivel[0]}</CustomTableTitleCell>
+              <CustomTableTextCell>
+                  {op_consultoria[0]}
+              </CustomTableTextCell>
+          </TableRow>
+          <TableRow>
+              <CustomTableTitleCell variant="head">{op_nivel[1]}</CustomTableTitleCell>
+              <CustomTableTextCell>
+                  {op_consultoria[1]}
+              </CustomTableTextCell>
+          </TableRow>
+          <TableRow>
+              <CustomTableTitleCell variant="head">{op_nivel[2]}</CustomTableTitleCell>
+              <CustomTableTextCell>
+                  {op_consultoria[2]}
+              </CustomTableTextCell>
+          </TableRow>
+          <TableRow>
+              <CustomTableTitleCell variant="head">{op_nivel[3]}</CustomTableTitleCell>
+              <CustomTableTextCell>
+                  {op_consultoria[3]}
+              </CustomTableTextCell>
+          </TableRow>
+          </TableBody>
+      </Table>
+  </TableContainer>
+  );
+}
+
+export default TabelaCursos;
