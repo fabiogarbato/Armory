3f0cb5207125422f520f6cb061006b9b22591e60
fabiogarbato <82621006+fabiogarbato@users.noreply.github.com>
2023-09-28 18:02:34 -0300

    Criação do estilo da página de ERRO

--- src/pages/Erro/erro.js
+++ src/pages/Erro/erro.js
@@ -1,13 +1,22 @@
 import './erro.css' 
+import { Link, useLocation } from 'react-router-dom';
 
 function Erro(){
-    return(
-        <div className='background'>
-            <div className='header'>
-                <div className='img_logo'></div>
+    const location = useLocation();
+    return(      
+        <div className='container'>
+            <div className='header_site'>  
+            <div className='img_logo'></div>
+                <div className='text-Container-header'>
+                    <Link to='/' className={`menu-button ${location.pathname === '/' ? 'active' : ''}`}>Página Inicial</Link>
+                    <Link to="/sobre" className={`menu-button ${location.pathname === '/sobre' ? 'active' : ''}`}>Sobre Nós</Link>
+                    <Link to="/contato" className={`menu-button ${location.pathname === '/contato' ? 'active' : ''}`}>Contato</Link>
+                </div>
             </div>
-            <h1 className='not-found'>404 Página não encontrada</h1>
-        </div>     
+            <div className='Content-container-error'>
+                <div className='text-error'>ERRO 404 - PÁGINA NÃO ENCONTRADA</div>  
+            </div>
+        </div>                      
     )
 }
 
