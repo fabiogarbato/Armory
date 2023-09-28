c3d8922fc1ce58cebbd3376a649c4d1bfc490345
fabiogarbato <82621006+fabiogarbato@users.noreply.github.com>
2023-09-28 19:59:56 -0300

    Ajustes no front da tabela

--- src/pages/Tabela/tabelaCursos.js
+++ src/pages/Tabela/tabelaCursos.js
@@ -19,23 +19,23 @@ function TabelaCursos() {
       <div className='Table-Content-container'>
         <div className="custom-table">
           <div className="custom-table-row header">
-            <div className="custom-table-cell">Nível</div>
-            <div className="custom-table-cell">Descrição</div>
+            <div className="custom-table-title-cell">Nível</div>
+            <div className="custom-table-title-cell">Descrição</div>
           </div>
           <div className="custom-table-row">
-            <div className="custom-table-cell">Básico</div>
+            <div className="custom-table-cell-nivel">Básico</div>
             <div className="custom-table-cell">Ideal para iniciantes acima de 18 anos, com baixa ou nenhuma experiência com armas de fogo. Optando por esporte ou defesa pessoal.</div>
           </div>
           <div className="custom-table-row">
-            <div className="custom-table-cell">Intermediário</div>
+            <div className="custom-table-cell-nivel">Intermediário</div>
             <div className="custom-table-cell">Para atiradores que já possuem um nível de conhecimento elevado de armas curtas. Querendo aprimorar seu aprendizado no emprego de armas portáteis.</div>
           </div>
           <div className="custom-table-row">
-            <div className="custom-table-cell">Avançado</div>
+            <div className="custom-table-cell-nivel">Avançado</div>
             <div className="custom-table-cell">Treinamento tático para civil ou forças armadas. Abrangendo destreza, disciplina e técnicas avançadas de armas semi-automáticas e de repetição.</div>
           </div>
           <div className="custom-table-row">
-            <div className="custom-table-cell">Consultoria</div>
+            <div className="custom-table-cell-nivel">Consultoria</div>
             <div className="custom-table-cell">Iniciamos os trâmites no momento do aceite do cliente em adquirir uma arma de fogo, até sua conclusão final. Com seriedade e competência.</div>
           </div>
         </div>
