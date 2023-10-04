5e1baeaef6f07d4fb92fd47cb0a2a1f568a3d5f3
ViniSiqueira <vinicius_siqueirabravo@hotmail.com>
2023-10-04 20:01:53 -0300

    ajuste de responsividade

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -7,18 +7,18 @@ function Home() {
   const location = useLocation();
   return (
     <div className='container'>
-      <Navigation/> 
-      <Link to='/tabela' className={`img-container1 ${location.pathname === '/tabela' ? 'active' : ''}`}>Nossos Cursos</Link>
-      <Link to='/estrutura' className={`img-container2 ${location.pathname === '/estrutura' ? 'active' : ''}`}>Nossa Estrutura</Link>
-      <div className='text-img-container'>
-        <div className='column-1'>
-          <div className='text-name_image1'>Nossos Cursos</div>        
-        </div>
-        <div className='column-2'>
-          <div className='text-name_image2'>Nossa Estrutura</div>        
-        </div>
+    <Navigation /> 
+    <div className='text-img-container'>
+      <div className='container-left'>
+        <Link to='/tabela' className={`img-container1 ${location.pathname === '/tabela' ? 'active' : ''}`}/>
+        <div className='text-name_image1'>Nossos Cursos</div>            
+      </div>
+      <div className='container-right'>
+        <Link to='/estrutura' className={`img-container2 ${location.pathname === '/estrutura' ? 'active' : ''}`}/>
+        <div className='text-name_image2'>Nossa Estrutura</div>
       </div>
     </div>
+  </div>
   );
 }
 
