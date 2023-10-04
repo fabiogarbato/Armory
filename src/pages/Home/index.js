89d0db6b063b6d16f61d7e544b1b22dce59352c0
Fabio <fabio.garbato@mps.com>
2023-10-04 13:39:42 -0300

    Responsividade

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -12,10 +12,10 @@ function Home() {
       <Link to='/estrutura' className={`img-container2 ${location.pathname === '/estrutura' ? 'active' : ''}`}>Nossa Estrutura</Link>
       <div className='text-img-container'>
         <div className='column-1'>
-          <div className='text-name-img1'>Nossos Cursos</div>        
+          <div className='text-name_image1'>Nossos Cursos</div>        
         </div>
         <div className='column-2'>
-          <div className='text-name-img2'>Nossa Estrutura</div>        
+          <div className='text-name_image2'>Nossa Estrutura</div>        
         </div>
       </div>
     </div>
