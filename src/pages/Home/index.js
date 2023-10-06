9a7891c0394e9485011e99668dc1fa812403d005
fabiogarbato <82621006+fabiogarbato@users.noreply.github.com>
2023-10-05 21:14:07 -0300

    navbar

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -8,31 +8,31 @@ import Image from 'react-bootstrap/Image';
 function Home() {
 
   return (
-    <Navbar expand="lg" className="bg-body-tertiary">
+    <Navbar expand="lg" className="bg-primary" style={{ height: '150px' }}>
       <Container>
       <Navbar.Brand href="#home">
-        <Image src="../../images/logo.png" alt="Logo do Cliente" /> 
+        <Image src="../../images/logo.png" /> 
       </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="ms-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+        <Nav className="ms-auto">
+          <Nav.Link href="#home" style={{ fontSize: '30px', fontFamily: 'Korataki, serif' }}>Home</Nav.Link>
+          <Nav.Link href="#link" style={{ fontSize: '30px' }}>Link</Nav.Link>
+          <NavDropdown title="Dropdown" id="basic-nav-dropdown" style={{ fontSize: '30px' }}>
+            <NavDropdown.Item href="#action/3.1" >Action</NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.2">
+              Another action
+            </NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
               <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
+            <NavDropdown.Item href="#action/3.4">
                 Separated link
-              </NavDropdown.Item>
-            </NavDropdown>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+            </NavDropdown.Item>
+          </NavDropdown>
+        </Nav>
+      </Navbar.Collapse>
+    </Container>
+  </Navbar>
   );
 }
 
