89ee90506ab06f5133ff7da2163a33df00f303f7
fabiogarbato <82621006+fabiogarbato@users.noreply.github.com>
2023-10-05 23:19:37 -0300

    Estilização inicial da tela principal

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,10 +1,30 @@
 import Menubar from '../Menubar/menubar'
+import { Container } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 import './home.css';
 
 function Home() {
 
   return (
-    <Menubar/>
+    <div>
+      <Menubar />
+      <Container fluid>
+        <Row className="body flex-wrap">
+          <Col md={6} sm={12} xs={12} className="coluna1 d-flex justify-content-center align-items-center">
+            <Container className="container-imagem-simulada">
+              <p className="imagem-texto">Nossos Cursos</p>
+            </Container>
+          </Col>
+          <Col md={6} sm={12} xs={12} className="coluna2 d-flex justify-content-center align-items-center flex-column">
+            <Container className="container-imagem-simulada2" >
+              <p className="imagem-texto">Nossa Estrutura</p>
+            </Container>
+          </Col>
+        </Row>
+      </Container>
+    </div>
   );
 }
 
