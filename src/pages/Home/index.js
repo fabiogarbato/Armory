792243e4176f55d0df8e7e5d789c42dd70e41581
fabiogarbato <82621006+fabiogarbato@users.noreply.github.com>
2023-10-06 19:23:37 -0300

    Inclusão das imagens no menu principal

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -3,24 +3,23 @@ import { Container } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import Image from 'react-bootstrap/Image';
+import Arma1 from '../../images/arma1.jpg';
+import Arma2 from '../../images/arma2.jpg';
 import './home.css';
 
 function Home() {
 
   return (
     <div>
-      <Menubar />
+      <Menubar/>
       <Container fluid>
         <Row className="body flex-wrap">
-          <Col md={6} sm={12} xs={12} className="coluna1 d-flex flex-column justify-content-center align-items-center">
-            <Container className="container-imagem-simulada">
-              <p className="imagem-texto">Nossos Cursos</p>
-            </Container>
+          <Col lg={6} md={6} sm={12} className="coluna d-flex flex-column justify-content-center align-items-center">
+            <Image src={Arma1} alt="Imagem 1" className="img-fluid imagem-custom" />
           </Col>
-          <Col md={6} sm={12} xs={12} className="coluna2 d-flex flex-column justify-content-center align-items-center">
-            <Container className="container-imagem-simulada2">
-              <p className="imagem-texto">Nossa Estrutura</p>
-            </Container>
+          <Col lg={6} md={6} sm={12} className="coluna d-flex flex-column justify-content-center align-items-center">
+            <Image src={Arma2} alt="Imagem 2" className="img-fluid imagem-custom" />
           </Col>
         </Row>
       </Container>
--- src/pages/Menubar/menubar.js
+++ src/pages/Menubar/menubar.js
@@ -12,24 +12,24 @@ function Menubar() {
   return (
     <Navbar expand="lg" className="background-color" style={{ height: '300px' }}>
       <Container>
-      <Navbar.Brand href="#home">
-        <Image src="../../images/logo.png" alt="Armory" />
-      </Navbar.Brand>
+        <Navbar.Brand href="#home">
+          <Image src="../../images/logo.png" alt="Armory" />
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" className="custom-menu-icon"/>
         <Navbar.Collapse id="basic-navbar-nav">
-        <Nav className="ms-auto">
-          <Link to="/" className="text-color link-style" style={{ fontSize: '30px', fontFamily: 'Korataki, serif', marginLeft: '30px', marginTop: '-80px' }}>Pagina Inicial</Link>
-          <Link to="/sobre" className="text-color link-style" style={{ fontSize: '30px', fontFamily: 'Korataki, serif', marginLeft: '30px', marginTop: '-80px' }}>Sobre Nós</Link>
-          <Link to="/contato" className="text-color link-style" style={{ fontSize: '30px', fontFamily: 'Korataki, serif', marginLeft: '30px', marginTop: '-80px' }}>Contato</Link>
-          <NavDropdown title="Parceiros" id="basic-nav-dropdown" className="custom-dropdown-item custom-dropdown-menu" style={{ fontSize: '30px', fontFamily: 'Korataki, serif', marginLeft: '30px', marginTop: '-86px'}}>
-            <Link to="/parceiro/obelico" className="text-color link-style">OBelico</Link>
-            <NavDropdown.Divider />
-            <Link to="/parceiro/cabana-das-armas" className="text-color link-style">Cabana Das Armas</Link>
-          </NavDropdown>
-        </Nav>
-      </Navbar.Collapse>
-    </Container>
-  </Navbar>
+          <Nav className="ms-auto">
+            <Link to="/" className="text-color link-style" style={{ fontSize: '30px', fontFamily: 'Korataki, serif', marginLeft: '30px', marginTop: '-80px' }}>Pagina Inicial</Link>
+            <Link to="/sobre" className="text-color link-style" style={{ fontSize: '30px', fontFamily: 'Korataki, serif', marginLeft: '30px', marginTop: '-80px' }}>Sobre Nós</Link>
+            <Link to="/contato" className="text-color link-style" style={{ fontSize: '30px', fontFamily: 'Korataki, serif', marginLeft: '30px', marginTop: '-80px' }}>Contato</Link>
+            <NavDropdown title="Parceiros" id="basic-nav-dropdown" className="custom-dropdown-item custom-dropdown-menu" style={{ fontSize: '30px', fontFamily: 'Korataki, serif', marginLeft: '30px', marginTop: '-86px'}}>
+              <Link to="/parceiro/obelico" className="text-color link-style">OBelico</Link>
+              <NavDropdown.Divider />
+              <Link to="/parceiro/cabana-das-armas" className="text-color link-style">Cabana Das Armas</Link>
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
 }
 
