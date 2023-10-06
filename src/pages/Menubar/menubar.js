1d7f72a33cb32c57410bb2b27f6829d96e08bacb
fabiogarbato <82621006+fabiogarbato@users.noreply.github.com>
2023-10-06 18:52:39 -0300

    Ajustes menubar

--- src/pages/Menubar/menubar.js
+++ src/pages/Menubar/menubar.js
@@ -1,4 +1,5 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Link, NavLink } from 'react-router-dom';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
@@ -17,15 +18,13 @@ function Menubar() {
         <Navbar.Toggle aria-controls="basic-navbar-nav" className="custom-menu-icon"/>
         <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="ms-auto">
-          <Nav.Link href="#home" className="text-color" style={{ fontSize: '30px', fontFamily: 'Korataki, serif', marginLeft: '30px', marginTop: '-90px' }}>Pagina Inicial</Nav.Link>
-          <Nav.Link href="#link" className="text-color" style={{ fontSize: '30px', fontFamily: 'Korataki, serif', marginLeft: '30px', marginTop: '-90px' }}>Sobre Nós</Nav.Link>
-          <Nav.Link href="#contato" className="text-color" style={{ fontSize: '30px', fontFamily: 'Korataki, serif', marginLeft: '30px', marginTop: '-90px' }}>Contato</Nav.Link>
-          <NavDropdown title="Parceiros" id="basic-nav-dropdown" className="custom-dropdown-item" style={{ fontSize: '30px', fontFamily: 'Korataki, serif', marginLeft: '30px', marginTop: '-90px'}}>
-            <NavDropdown.Item href="#action/3.1" className="text-color">OBelico</NavDropdown.Item>
-              <NavDropdown.Divider />
-            <NavDropdown.Item href="#action/3.4" className="text-color">
-                Cabana Das Armas
-            </NavDropdown.Item>
+          <Link to="/" className="text-color link-style" style={{ fontSize: '30px', fontFamily: 'Korataki, serif', marginLeft: '30px', marginTop: '-80px' }}>Pagina Inicial</Link>
+          <Link to="/sobre" className="text-color link-style" style={{ fontSize: '30px', fontFamily: 'Korataki, serif', marginLeft: '30px', marginTop: '-80px' }}>Sobre Nós</Link>
+          <Link to="/contato" className="text-color link-style" style={{ fontSize: '30px', fontFamily: 'Korataki, serif', marginLeft: '30px', marginTop: '-80px' }}>Contato</Link>
+          <NavDropdown title="Parceiros" id="basic-nav-dropdown" className="custom-dropdown-item custom-dropdown-menu" style={{ fontSize: '30px', fontFamily: 'Korataki, serif', marginLeft: '30px', marginTop: '-86px'}}>
+            <Link to="/parceiro/obelico" className="text-color link-style">OBelico</Link>
+            <NavDropdown.Divider />
+            <Link to="/parceiro/cabana-das-armas" className="text-color link-style">Cabana Das Armas</Link>
           </NavDropdown>
         </Nav>
       </Navbar.Collapse>
