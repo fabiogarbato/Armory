6b5d86aef14a63c1ef676d09db6b07589de2a810
fabiogarbato <82621006+fabiogarbato@users.noreply.github.com>
2023-10-05 22:05:03 -0300

    Estilização do header

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -4,29 +4,27 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import Image from 'react-bootstrap/Image';
+import './home.css';
 
 function Home() {
 
   return (
-    <Navbar expand="lg" className="bg-primary" style={{ height: '150px' }}>
+    <Navbar expand="lg" className="background-color" style={{ height: '300px' }}>
       <Container>
       <Navbar.Brand href="#home">
-        <Image src="../../images/logo.png" /> 
+        <Image src="../../images/logo.png" alt="Armory" />
       </Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Toggle aria-controls="basic-navbar-nav" className="custom-menu-icon"/>
         <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="ms-auto">
-          <Nav.Link href="#home" style={{ fontSize: '30px', fontFamily: 'Korataki, serif' }}>Home</Nav.Link>
-          <Nav.Link href="#link" style={{ fontSize: '30px' }}>Link</Nav.Link>
-          <NavDropdown title="Dropdown" id="basic-nav-dropdown" style={{ fontSize: '30px' }}>
-            <NavDropdown.Item href="#action/3.1" >Action</NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.2">
-              Another action
-            </NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+          <Nav.Link href="#home" className="text-color" style={{ fontSize: '30px', fontFamily: 'Korataki, serif', marginLeft: '30px' }}>Pagina Inicial</Nav.Link>
+          <Nav.Link href="#link" className="text-color" style={{ fontSize: '30px', fontFamily: 'Korataki, serif', marginLeft: '30px' }}>Sobre Nós</Nav.Link>
+          <Nav.Link href="#contato" className="text-color" style={{ fontSize: '30px', fontFamily: 'Korataki, serif', marginLeft: '30px' }}>Contato</Nav.Link>
+          <NavDropdown title="Parceiros" id="basic-nav-dropdown" style={{ fontSize: '30px', fontFamily: 'Korataki, serif', marginLeft: '30px', color: 'orange' }}>
+            <NavDropdown.Item href="#action/3.1" className="text-color">OBelico</NavDropdown.Item>
               <NavDropdown.Divider />
-            <NavDropdown.Item href="#action/3.4">
-                Separated link
+            <NavDropdown.Item href="#action/3.4" className="text-color">
+                Cabana Das Armas
             </NavDropdown.Item>
           </NavDropdown>
         </Nav>
