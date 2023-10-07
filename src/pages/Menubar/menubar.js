b434ef436ff7e19d80bea3a205e42f5f129a227d
fabiogarbato <82621006+fabiogarbato@users.noreply.github.com>
2023-10-06 21:02:56 -0300

    Correção Parceiros, para ficar na cor do menubar

--- src/pages/Menubar/menubar.js
+++ src/pages/Menubar/menubar.js
@@ -22,7 +22,7 @@ function Menubar() {
             <Link to="/" className="text-color link-style" style={{ fontSize: '30px', fontFamily: 'Korataki, serif', marginLeft: '30px', marginTop: '-80px' }}>Pagina Inicial</Link>
             <Link to="/sobre" className="text-color link-style" style={{ fontSize: '30px', fontFamily: 'Korataki, serif', marginLeft: '30px', marginTop: '-80px' }}>Sobre Nós</Link>
             <Link to="/contato" className="text-color link-style" style={{ fontSize: '30px', fontFamily: 'Korataki, serif', marginLeft: '30px', marginTop: '-80px' }}>Contato</Link>
-            <NavDropdown title="Parceiros" id="basic-nav-dropdown" className="custom-dropdown-item custom-dropdown-menu" style={{ fontSize: '30px', fontFamily: 'Korataki, serif', marginLeft: '30px', marginTop: '-86px'}}>
+            <NavDropdown title={<span style={{ color: '#ff7300' }}>Parceiros</span>} id="basic-nav-dropdown" className="custom-dropdown-item custom-dropdown-menu" style={{ fontSize: '30px', fontFamily: 'Korataki, serif', marginLeft: '30px', marginTop: '-86px'}}>
               <Link to="/parceiro/obelico" className="text-color link-style">OBelico</Link>
               <NavDropdown.Divider />
               <Link to="/parceiro/cabana-das-armas" className="text-color link-style">Cabana Das Armas</Link>
