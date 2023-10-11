0ef8c5c08121829ae95faeb05da59defcdc333b4
Fabio <fabio.garbato@mps.com>
2023-10-11 17:02:21 -0300

    Criação da tela de erro

--- src/pages/Erro/erro.js
+++ src/pages/Erro/erro.js
@@ -1,9 +1,21 @@
 import './erro.css' 
-import React, {useState} from 'react';
+import Menubar from '../Menubar/menubar'
+import React from 'react';
+import { Container } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 function Erro(){
     return(      
-       <></>                  
+        <div>
+            <Menubar />
+            <Container fluid style={{ position: 'relative', height: '60vh', display: 'flex', justifyContent: 'center' }}>
+                <Row className="align-items-center">
+                    <Col className='error-text'>ERRO - 404 PAGINA NÃO ENCONTRADA</Col>
+                </Row>
+            </Container>
+        </div>                
     )
 }
 
