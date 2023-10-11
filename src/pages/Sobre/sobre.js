45e863bd92bd5b5c6d9d72b06534800cfc487a42
Fabio <fabio.garbato@mps.com>
2023-10-11 17:40:05 -0300

    sobre

--- src/pages/Sobre/sobre.js
+++ src/pages/Sobre/sobre.js
@@ -10,9 +10,19 @@ function Sobre() {
   return (
     <div>
       <Menubar />
-      <Container fluid style={{ position: 'relative', height: '5vh', display: 'flex', justifyContent: 'center' }}>
+      <Container fluid style={{ backgroundColor: 'pink', position: 'relative', height: '5vh', display: 'flex', justifyContent: 'center' }}>
         <Row style={{marginBottom: '0' }} className="align-items-center">
           <Col className='title-text'>Sobre Nós</Col>
+        </Row>
+      </Container>
+      <Container fluid style={{ backgroundColor: 'pink', position: 'relative', height: '20vh', display: 'flex', justifyContent: 'center', marginTop: '10px' }}>
+        <Row style={{ marginBottom: '0' }} className="align-items-center">
+          <Col xs={6} className="bg-blue text-white text-center d-flex align-items-top" style={{ backgroundColor: 'blue', height: '20vh', width: '100vh'}}>
+            Azul
+          </Col>
+          <Col xs={6} className="bg-red text-white text-center d-flex align-items-top" style={{ backgroundColor: 'red', height: '20vh', width: '100vh'}}>
+            Vermelho
+          </Col>
         </Row>
       </Container>
     </div>
