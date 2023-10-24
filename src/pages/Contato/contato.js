3e35ff4dfeca438d86dd7521b5ed4a8c2f589920
Fabio <fabio.garbato@mps.com>
2023-10-24 13:26:45 -0300

    tela contato

--- src/pages/Contato/contato.js
+++ src/pages/Contato/contato.js
@@ -18,17 +18,17 @@ function Contato() {
           <Col className='title-text'>Contato</Col>
         </Row>
       </Container>
-      <Container fluid style={{ position: 'relative' }}>
+      <Container fluid style={{ position: 'relative', margin:'0' }}>
         <Row className="body flex-wrap">
-          <Col lg={6} md={6} sm={12} className="coluna d-flex flex-column justify-content-center align-items-center">
-            <p className='text-center'>@armoryconsulting</p>
-            <a href="/tabela">
-              <Image src={Instagram} alt="Imagem 1" className="img-fluid imagem-social" />
+          <Col lg={6} md={6} sm={12} style={{  marginBottom: '20%' }} className="coluna d-flex flex-column justify-content-center align-items-center">
+            <p style={{ marginBottom: '8%'}} className='text-center'>@armoryconsulting</p>
+            <a href="https://www.instagram.com/armoryconsulting/" target="_blank">
+              <Image src={Instagram} alt="Imagem 1" className="img-fluid imagem-social-insta" />
             </a>
           </Col>
-          <Col lg={6} md={6} sm={12} className="coluna d-flex flex-column justify-content-center align-items-center">
+          <Col lg={6} md={6} sm={12} style={{ marginBottom: '19%' }} className="coluna d-flex flex-column justify-content-center align-items-center">
               <p className='text-center'>Armory Consulting</p>
-              <a href="/tabela">
+              <a href="https://www.facebook.com/armoryconsulting/" target="_blank">
                 <Image src={Facebook} alt="Imagem 2" className="img-fluid imagem-social" />
               </a>
           </Col>
