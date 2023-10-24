11c62a12415797f6b441c1e00bf55224e5772cd7
Fabio <fabio.garbato@mps.com>
2023-10-24 13:57:53 -0300

    Responsividade tela Contato

--- src/pages/Contato/contato.js
+++ src/pages/Contato/contato.js
@@ -21,13 +21,13 @@ function Contato() {
       <Container fluid style={{ position: 'relative', margin:'0' }}>
         <Row className="body flex-wrap">
           <Col lg={6} md={6} sm={12} style={{  marginBottom: '20%' }} className="coluna d-flex flex-column justify-content-center align-items-center">
-            <p style={{ marginBottom: '8%'}} className='text-center'>@armoryconsulting</p>
+            <p style={{ marginBottom: '8%'}} className='text-center-contact'>@armoryconsulting</p>
             <a href="https://www.instagram.com/armoryconsulting/" target="_blank">
               <Image src={Instagram} alt="Imagem 1" className="img-fluid imagem-social-insta" />
             </a>
           </Col>
           <Col lg={6} md={6} sm={12} style={{ marginBottom: '19%' }} className="coluna d-flex flex-column justify-content-center align-items-center">
-              <p className='text-center'>Armory Consulting</p>
+              <p className='text-center-contact'>Armory Consulting</p>
               <a href="https://www.facebook.com/armoryconsulting/" target="_blank">
                 <Image src={Facebook} alt="Imagem 2" className="img-fluid imagem-social" />
               </a>
