a553c200a8ed7a8c206e9fd5c58ee5c71a465b34
Fabio <fabio.garbato@mps.com>
2023-10-25 13:38:21 -0300

    Esboço pagina "Sobre"

--- src/pages/Sobre/sobre.js
+++ src/pages/Sobre/sobre.js
@@ -15,13 +15,15 @@ function Sobre() {
           <Col className='title-text'>Sobre Nós</Col>
         </Row>
       </Container>
-      <Container fluid className='container-sobre'>
-        <Row style={{ marginBottom: '0' }} className="align-items-center">
-          <Col xs={6} sm={6} md={6} lg={6} className="bg-blue text-white text-center d-flex align-items-top column1">
-            <p className='text-style'>UMA BREVE APRESENTAÇÃO</p>
+      <Container fluid style={{ position: 'relative', margin:'0', height: '30vh'}}>
+        <Row className="body flex-wrap">
+          <Col lg={6} md={6} sm={12} style={{ marginBottom: '46%' }} className="coluna d-flex flex-column justify-content-center align-items-center">
+            <p className='text-center-sobre'>UMA BREVE APRESENTAÇÃO</p>
+            <h3 className='text-sobre-content'>Somos uma empresa de consultoria e treinamento. Nosso principal foco é capacitar clientes com a nossa expertise. Compartilhar experiências e principalmente ensinar nossas técnicas do nível básico ao avançado.</h3>
           </Col>
-          <Col xs={6} sm={6} md={6} lg={6} className="bg-red text-white text-center d-flex align-items-top column2">
-            <p className='text-style'>MUITA PESQUISA E PROFISSIONALISMO</p> 
+          <Col lg={6} md={6} sm={12} style={{ marginBottom: '40%' }} className="coluna d-flex flex-column justify-content-center align-items-center">
+              <p className='text-center-sobre'>MUITA PESQUISA E PROFISSIONALISMO</p>
+              <h3 className='text-sobre-content'>O cidadão brasileiro é prejudicado reiteradamente com informações desencontradas no quesito arma de fogo. Por esta razão, criamos um projeto inovador ao realizar um levantamento mais assertivo. Com uma base sólida de conhecimento, a Armory Consulting foi criada para promover consultoria, cursos e treinamento no crescente mercado nacional de armas.</h3>
           </Col>
         </Row>
       </Container>
